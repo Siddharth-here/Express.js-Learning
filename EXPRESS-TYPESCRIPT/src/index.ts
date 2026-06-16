6ffd55f18f214e5b49a00c42331501296aaf7c3c
Siddharth-here <siddharthmohapatra2004@gmail.com>
2026-06-16 23:53:47 +0530

    new file added

--- EXPRESS-TYPESCRIPT/src/index.ts
+++ EXPRESS-TYPESCRIPT/src/index.ts
@@ -1,18 +1,23 @@
 //create a http server and running on a port
 
-import http from 'node:http'
-import { env } from './env.js'
-import { createServerApplication } from './app/index.js'
+import http from 'node:http' //here we are importing Nodes 'http' module
 
-async function main(){
-    try {
+import { env } from './env.js' //here we are calling object named {env} from a local folder named env.js. This is where the code looks up configuration like what port number to use.
+
+import { createServerApplication } from './app/index.js' //here we are importing a function 'createServerApplication' from app folder that holds the core logic of how the app actually process incoming web requests.
+
+
+async function main(){ //handle tasks that takes time to complete without freezing the entire server.
+
+    try { //attempt to run the following code 
+        
         const server = http.createServer(createServerApplication())
         const PORT: number = env.PORT ? +env.PORT: 8080
 
         server.listen(PORT, () =>{
             console.log(`Server is running on PORT ${PORT}`)
         })
-    } catch (error) {
+    } catch (error) { //safety net
         throw error
         
     }
