e73f12de52d903faa3cbfb04d233c6d0010f0520
Siddharth-here <siddharthmohapatra2004@gmail.com>
2026-06-19 22:52:14 +0530

    updated

--- 03-middleware.js
+++ 03-middleware.js
@@ -1,108 +1,139 @@
-const express = require('express')
+const express = require("express");
 
+function block_1_httpMethods() {
+  return new Promise((resolve) => {
+    const app = express();
 
-function block_1_httpMethods(){
-    return new Promise((resolve) => {
-        const app = express()
-        app.use(express.json())
+    const logs = [];
+    app.use(express.json()); //middleware
 
-        
+    //request Logger
+    app.use((req, res, next) => {
+      //buisness logic
+      const logEntry = `${req.method} : ${req.url}`;
+      logs.push(logEntry);
+      console.log(`[LOG] -- ${logEntry}`);
+      //if your requesthangs forever
+      next();
+    });
 
+    app.use((req, res, next) => {
+      req.startTime = Date.now();
 
+      res.on("finish", () => {
+        const duration = Date.now() - req.startTime;
+        console.log(
+          `[TIMER] - ${Request.method} - ${req.url} took ${duration}ms`,
+        );
+      });
 
-        const server = app.listen(0, async () => {
-            const port = server.address().port
-            const base = `http://127.0.0.1:${port}`;
+      next();
+    });
 
-            try {
-                //TODO
-                const listRes = await fetch(`${base}/routes`)
-                const listData = await listRes.json()
+    //custom middleware
+    function authMe (req, res, next) {
+       const token = req.headers['x-auth-token']
 
-                const createRes = await fetch(`${base}/routes`, {
-                    method: "POST",
-                    headers: {
-                        'Content-Type': "application/json",
-                        body: JSON.stringify({
-                            name: "Colaba-Worli",
-                            direction: "South"
-                        })
-                    }
-                })
-                const created = await createRes.json()
+       if(!token){
+        return res.status(401).json({"error": "No token, please login"})
+       }
 
-                
-            } catch (error) {
-                console.log(error)
+       if(token !== "secret"){
+        return res.status(403).json({error: "Invalid token"})
+       }
+       
+       //token -> extract data from token -> userID, email, admin
+       req.user = {id:1, name:"sidd", role:"dev"}
+        next()
+    }
+
+    function getRole(role){
+        return(req, res, next)=>{
+            if(!req.user || req.user.role !== role){
+                return res.status(403).json({error: `Role ${role} REQUIRED`})
             }
+            next()
+        }
+    }
 
-            server.close(() => {
-                console.log("Block 1 served....")
-                resolve()
-            })
+    app.get('/profile', authMe, ()=>{})
 
+    const server = app.listen(0, async () => {
+      const port = server.address().port;
+      const base = `http://127.0.0.1:${port}`;
 
-        })
+      try {
+        //TODO
+        const listRes = await fetch(`${base}/routes`);
+        const listData = await listRes.json();
 
+        const createRes = await fetch(`${base}/routes`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            body: JSON.stringify({
+              name: "Colaba-Worli",
+              direction: "South",
+            }),
+          },
+        });
+        const created = await createRes.json();
+      } catch (error) {
+        console.log(error);
+      }
 
-    })
+      server.close(() => {
+        console.log("Block 1 served....");
+        resolve();
+      });
+    });
+  });
 }
 
+function block_1_httpMethods() {
+  return new Promise((resolve) => {
+    const app = express();
+    app.use(express.json());
+    // /files/docs/readme.txt
+    // /files/assets/style.css
+    app.get("/files/*filepath", (req, res) => {
+      const filepath = req.params.filepath;
+      res.json({ filepath, type: "wildcard" });
+    });
 
-function block_1_httpMethods(){
-    return new Promise((resolve) => {
-        const app = express()
-        app.use(express.json())
-// /files/docs/readme.txt
-// /files/assets/style.css
-        app.get('/files/*filepath', (req, res) => {
-            const filepath = req.params.filepath
-            res.json({filepath, type: "wildcard"})
-        })
+    app
+      .route("/schedule")
+      .get((req, res) => {})
+      .post((req, res) => {})
+      .put((req, res) => {})
+      .delete((req, res) => {});
 
-        app
-            .route("/schedule")
-            .get((req, res) => {})
-            .post((req, res) => {})
-            .put((req, res) => {})
-            .delete((req, res) => {})
+    app.use("/api", (req, res) => {
+      //its a prefetch match
+    });
 
-        app.use("/api", (req, res) => {
-            //its a prefetch match
-        })
+    const server = app.listen(0, async () => {
+      const port = server.address().port;
+      const base = `http://127.0.0.1:${port}`;
 
+      try {
+        //TODO
+      } catch (error) {
+        console.log(error);
+      }
 
-
-        const server = app.listen(0, async () => {
-            const port = server.address().port
-            const base = `http://127.0.0.1:${port}`;
-
-            try {
-                //TODO
-                
-
-                
-            } catch (error) {
-                console.log(error)
-            }
-
-            server.close(() => {
-                console.log("Block 1 served....")
-                resolve()
-            })
-
-
-        })
-
-
-    })
+      server.close(() => {
+        console.log("Block 1 served....");
+        resolve();
+      });
+    });
+  });
 }
 
-async function main(){
-    await block_1_httpMethods()
+async function main() {
+  await block_1_httpMethods();
 
-
-    process.exit(0)
+  process.exit(0);
 }
 
-main()
+main();
